2327d6dfa0493e15c3b654441e5ce67fc396b87e
KBalint2003 <112862164+KBalint2003@users.noreply.github.com>
2024-01-06 19:24:50 +0100

    MVC modell implementálása, újradolgozás
    A backend szerver újragondolása, az MVC modell implementálása. A jelenlegi probléma: nem kapja meg az adatokat a poston küldött JSON állományból.

---
+++ Backend/src/models/megkotesek.js
@@ -0,0 +1,59 @@
+let megkotesek = {};
+
+const regex = "[\-\'A-Za-z0-9áéíóöőúüűÁÉÍÓÖŐÚÜŰ ]+";
+
+megkotesek.felhasznalonev = () => {
+    
+    const megkotes = {
+        'presence': {
+            allowEmpty: false
+        },
+        'type': 'string',
+        'length':{
+            'minimum': 5,
+            'maximum':32
+        },
+        'format': {
+            'pattern' : regex,
+            'flags' : 'i',
+            'message':'A felhasználónévnek a következő sémát kell követnie: '+ regex
+        }
+    }
+
+    return megkotes;
+}
+
+megkotesek.email = () => {
+
+    const megkotes = {
+        'presence': {
+            allowEmpty: false
+        },
+        'type': 'string',
+        'email':true
+    }
+
+    return megkotes;
+
+}
+
+megkotesek.jelszo = () => {
+    const megkotes = {
+        'presence': {
+            allowEmpty: false
+        },
+        'type': 'string',
+        'length':{
+            'minimum': 10
+        },
+        'format': {
+            'pattern' : regex,
+            'flags' : 'i',
+            'message':'A jelszónak a következő sémát kell követnie: '+ regex
+        }
+    }
+
+    return megkotes;
+}
+
+module.exports = megkotesek;
