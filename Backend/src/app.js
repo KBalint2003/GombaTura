0e74b5ffcade9cb4d63fca5a84014f6c8819ec91
KBalint2003 <112862164+KBalint2003@users.noreply.github.com>
2024-01-16 08:25:44 +0100

    Adatbázissal való sikeres összekapcsolás

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,10 +15,23 @@ app.use(express.json());
 
 app.use("/",regisztracioRouter);
 
-sequelize.kapcsolat();
 
+//kapcsolat function, segítségével csatlakoztatni tudjuk az adatbázist a backend szerverrel, a backend csak akkor indul el, ha a bach-db közötti kapcsolat sikeres.
+async function kapcsolat() {
+    try {
+        await sequelize.authenticate();
+        console.log('Sikeres kapcsolat az adatbázissal');
+
+        app.listen(PORT, () => {
+            console.log(`A szerver elindult és elérhető a http://localhost:${PORT} URL-en!`)
+        })
+      } 
+      catch (error) {
+        console.error('Nem sikerült csatlakozni az adatbázishoz.', error);
+      }
+}
 
 //A HTTP Szerver indítása a 3000-es porton
-app.listen(PORT, () => {
-    console.log(`A szerver elindult és elérhető a http://localhost:${PORT} URL-en!`)
-})
+kapcsolat();  
+
+
--- Backend/src/models/adatbazisKapcsolat.model.js
+++ Backend/src/models/adatbazisKapcsolat.model.js
@@ -1,18 +1,8 @@
 const { Sequelize } = require('sequelize');
-const { async } = require('validate.js');
 
 const sequelize = new Sequelize('user-login','root','',{
     host: 'localhost',
     dialect: 'mysql'
 })
 
-async function kapcsolat() {
-    try {
-        await sequelize.authenticate();
-        console.log('Sikeres kapcsolat az adatbázissal');
-      } catch (error) {
-        console.error('Nem sikerült csatlakozni az adatbázishoz.', error);
-      }
-}
-
-module.exports = {sequelize, kapcsolat};
+module.exports = sequelize;
