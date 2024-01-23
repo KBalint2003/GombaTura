9df5e0eb6ff192de4c8dc4f30fa2a665c131cdd4
KBalint2003 <112862164+KBalint2003@users.noreply.github.com>
2024-01-23 11:05:24 +0100

    Adatbázis Összekapcsolás, modellek

--- Backend/src/models/adatbazisKapcsolat.model.js
+++ Backend/src/models/adatbazisKapcsolat.model.js
@@ -2,7 +2,10 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('zemplen_eheto_gombai','root','',{
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    define: {
+        freezeTableName: true,
+    }
 })
 
 module.exports = sequelize;
--- Backend/src/models/felhasznalo.model.js
+++ Backend/src/models/felhasznalo.model.js
@@ -1,41 +1,45 @@
 const { Sequelize } = require("sequelize");
 const sequelize = require("./adatbazisKapcsolat.model");
+const { DataTypes } = Sequelize;
 
 const Felhasznalo = sequelize.define('belepes', {
     
     User_id: {
-        type: Sequelize.Datatypes.STRING,
+        type: DataTypes.STRING(32),
         allowNull: false,
+        primaryKey: true
     },
     
     Felhasznalonev: {
-        type: Sequelize.Datatypes.STRING,
+        type: DataTypes.STRING,
         allowNull: false,
     },
 
     Jelszo: {
-        type: Sequelize.Datatypes.STRING,
+        type: DataTypes.STRING,
         allowNull: false,
+
     },
 
     Email: {
-        type: Sequelize.Datatypes.STRING,
+        type: DataTypes.STRING,
         allowNull: false,
     },
 
     Szuletesi_ido: {
+        type: DataTypes.DATEONLY,
         allowNull: false,
+        
     },
 
     Telefon_szam: {
         allowNull: false,
     }
 
-},
-
-{
-    freezeTableName: true,
-    timestamps: false
 });
 
-await Felhasznalo.sync({alter: true});
+await Felhasznalo.sync({alter: true}).then(()=> {
+
+}).catch(() => {
+    
+});
